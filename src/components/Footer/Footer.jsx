0be44d2dc8a3dbf06e7ab6de2f341716da34798c
laura alburez <lauraalburez2017@gmail.com>
2026-01-18 19:36:10 -0500

    self edits no tutor

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h2 className="footer__author">Developed by Laura Lysaght</h2>
+      <h3 className="footer__year">2025</h3>
+    </div>
+  );
+}
+
+export default Footer;
